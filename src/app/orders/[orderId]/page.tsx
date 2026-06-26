5563a8d2ce053838df7a008eb3d55286eb02f20d
Omar Zhadykov <omar3282088@gmail.com>
2026-06-26 23:04:41 +0200

    feat: added extracted input fields to the details form

--- src/app/orders/[orderId]/page.tsx
+++ src/app/orders/[orderId]/page.tsx
@@ -3,15 +3,33 @@ import Link from "next/link";
 export default async function Page({
   params,
 }: {
-  params: Promise<{ slug: string }>;
+  params: Promise<{ orderId: string }>;
 }) {
-  const { slug } = await params;
+  const { orderId } = await params;
 
   return (
-    <div>
-      <Link href={`/orders/${slug}/details`}>Details</Link>;
-      <Link href={`/orders/${slug}/details`}>Status</Link>;
-      <Link href={`/orders/${slug}/details`}>Request Help</Link>;
+    <div className="space-y-4 p-8">
+      <h1 className="text-xl font-bold">Vorgang: {orderId}</h1>
+      <div className="flex flex-col space-y-2">
+        <Link
+          href={`/orders/${orderId}/details`}
+          className="text-blue-600 underline"
+        >
+          → Details (Dynamic JSON Form)
+        </Link>
+        <Link
+          href={`/orders/${orderId}/details`}
+          className="pointer-events-none text-slate-400"
+        >
+          Status Check
+        </Link>
+        <Link
+          href={`/orders/${orderId}/details`}
+          className="pointer-events-none text-slate-400"
+        >
+          Request Help
+        </Link>
+      </div>
     </div>
   );
 }
